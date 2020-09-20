018e1f9b10db892f1881d285c9334ca3339c5c62
dannyLiu <h152074716@gmail.com>
2020-09-20 23:42:22 +0800

    建立DateController，嘗試 return View()、return Content()

---
+++ WebApplication8/Controllers/DateController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class DateController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Time()
+        {
+            return Content(DateTime.Now.ToString());
+        }
+    }
+}
