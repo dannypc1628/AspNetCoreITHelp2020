499cd417e1be4cc90ac197a51c1794389f1bd0f3
dannyLiu <h152074716@gmail.com>
2020-10-01 00:02:39 +0800

    建立ProductController/GetProductAction與檢視，並在Privacy.cshtml設定連結

---
+++ WebApplication8/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AspCoreIThelp2020Context _context;
+        public ProductController(AspCoreIThelp2020Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GetProduct(int ID)
+        {
+            var data = _context.Product.FirstOrDefault(d => d.Id == ID);
+
+            return View(data);
+        }
+    }
+}
