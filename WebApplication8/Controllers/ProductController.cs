6242fbaf55d9e29866e3186c911e4611a27edabc
dannyLiu <h152074716@gmail.com>
2020-10-01 23:59:07 +0800

    建立Product資料的新增、查詢、刪除、修改等CRUD

--- WebApplication8/Controllers/ProductController.cs
+++ WebApplication8/Controllers/ProductController.cs
@@ -15,11 +15,80 @@ namespace WebApplication8.Controllers
             _context = context;
         }
 
+        public IActionResult Index()
+        {
+            var data = _context.Product.ToList();
+
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            var last = _context.Product.OrderByDescending(d=>d.Id).FirstOrDefault();
+            product.Id = last.Id + 1;
+            _context.Product.Add(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
         public IActionResult GetProduct(int ID)
+        {
+            var data = _context.Product.FirstOrDefault(d => d.Id == ID);
+            
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int ID)
         {
             var data = _context.Product.FirstOrDefault(d => d.Id == ID);
 
             return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)//(int ID,string Name,decimal Price)
+        {
+            var data = _context.Product.FirstOrDefault(d => d.Id == product.Id);
+
+            if (data != null)
+            {
+                data.Name = product.Name;
+                data.Price = product.Price;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int ID)
+        {
+            var data = _context.Product.FirstOrDefault(d => d.Id == ID);
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult DoDelete(int ID)
+        {
+            var data = _context.Product.FirstOrDefault(d => d.Id == ID);
+
+            if (data != null)
+            {
+                _context.Product.Remove(data);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
